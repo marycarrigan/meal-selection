990a6a084997fb850e16a0e25bd4b44b2473a406
marycarrigan4 <mary4@rams.colostate.edu>
2021-02-02 23:09:18 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import backgrnd from './images/vegetables.jpg';
 import './App.css';
+import {AppBar, Typography, Dialog, DialogTitle, DialogContent, Card, Container, CardHeader, CardContent} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+    constructor(props){
+         super(props);
+         this.state = {
+             dialogOpen: true
+         }
+    }
+  render() {
+    return (
+        <>
+           <AppBar position="static" style={{ backgroundImage: `url(${backgrnd})` }}><Typography variant="h2"><b>Meals by Mary</b></Typography></AppBar>
+           <Dialog open={this.state.dialogOpen} onClose={() => this.setState({dialogOpen: false})}>
+               <DialogTitle>Happy Birthday, Mom!</DialogTitle>
+               <DialogContent>Welcome to "Meals by Mary". Select the meals you would like me, your wonderful gourmet chef daughter, to prepare for you!</DialogContent>
+           </Dialog>
+
+           <Card>
+                <CardHeader title={"Recipe"}/>
+                <CardContent><Typography variant="body2">
+                   Poop, carefully extruded on a stick.
+                </Typography></CardContent>
+           </Card>
+        </>
+    );
+  }
 }
 
-export default App;
