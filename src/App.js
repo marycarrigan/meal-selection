35cde194eb6e9ec978bc494699d428cb2d5685b8
marycarrigan4 <mary4@rams.colostate.edu>
2021-02-10 08:33:01 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import backgrnd from './images/vegetables.jpg';
 import './App.css';
-import {AppBar, Typography, Dialog, DialogTitle, DialogContent, Checkbox} from '@material-ui/core';
+import {AppBar, Typography, Dialog, DialogTitle, DialogContent, Checkbox, Button} from '@material-ui/core';
 import choices from "./choices.js";
 import {Card, CardColumns, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Navbar, Row, Col, ListGroup, ListGroupItem} from 'reactstrap';
 import one from "./images/shrimp_chowder.jpg";
@@ -9,6 +9,8 @@ import two from "./images/steak_zoodles.jpg";
 import three from "./images/cauliflower_fried_rice.jpg";
 import four from "./images/bacon_asparagus_frittata.jpg";
 import five from "./images/salmon_spinach_tomato.jpg"
+
+import * as emailjs from 'emailjs-com'
 
 
 export default class App extends Component {
@@ -23,6 +25,10 @@ export default class App extends Component {
         this.images = [one, two, three, four, five];
         this.toggleSelected = this.toggleSelected.bind(this);
         this.numSelected = 0;
+        this.getSelectedForEmail = this.getSelectedForEmail.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.sendFeedback = this.sendFeedback.bind(this);
+        emailjs.init("user_ioTv4yrmRUSXiRH6HI0zV");
     }
 
     render() {
@@ -31,7 +37,7 @@ export default class App extends Component {
                <AppBar position="static" style={{ backgroundImage: `url(${backgrnd})` }}><Typography variant="h2"><b>Meals by Mary</b></Typography></AppBar>
                <Dialog open={this.state.dialogOpen} onClose={() => this.setState({dialogOpen: false})}>
                    <DialogTitle>Happy Birthday, Mom!</DialogTitle>
-                   <DialogContent>Welcome to "Meals by Mary". Select the meals you would like me, your wonderful gourmet chef daughter, to prepare for you!</DialogContent>
+                   <DialogContent>Welcome to "Meals by Mary". Select the meals you would like me, your wonderful gourmet chef daughter, to prepare for you! More recipes will be added soon!</DialogContent>
                </Dialog>
                 <Navbar color="light" light expand="md"><h2>Selected</h2></Navbar>
                 <Row>
@@ -42,11 +48,13 @@ export default class App extends Component {
                                             <ListGroup>
                                                 <ListGroupItem>{item.title}</ListGroupItem>
                                             </ListGroup>)}})}
+                        {this.renderSubmitChoicesButton()}
                     </Col>
                 </Row>
                 <Navbar color="light" light expand="md"><h2>Choices</h2></Navbar>
                 <Row>
                     <Col sm={12} md={{size: 10, offset: 1}}>
+                        <div style={{margin: "10px"}}>{this.renderHateThisButton()}</div>
                         <CardColumns>
                             {this.state.items.map((item, index) => {
                                     return(
@@ -68,15 +76,77 @@ export default class App extends Component {
         );
   }
 
-  toggleSelected(index){
-        if(this.numSelected == 3){
-            alert("Please only select 3 meals.");
-            return;
+  renderSubmitChoicesButton(){
+        if(this.numSelected !== 0){
+            return(
+                <div style={{margin: "10px"}}><Button variant="outlined" color="primary" onClick={() => this.handleSubmit()}>Submit Choices</Button></div>
+            )
         }
-        this.selected[index] = !this.selected[index];
-        this.numSelected++;
+  }
+
+    handleSubmit () {
+        const message = this.getSelectedForEmail();
+        if(message !== ""){
+            const template_params = {
+                chosen_meals: `${message}`,
+                from_name: "Meal Selection Site",
+                reply_to: "marykcarrigan@gmail.com"
+            };
+            this.sendFeedback('template_9k48ken', template_params)
+        }
+        alert("Mary has been notified of your selections!");
+    }
+
+    sendFeedback (templateId, variables) {
+        emailjs.send(
+            'service_6jn98un', templateId,
+            variables
+        ).then(res => {
+            console.log('Email successfully sent!')
+        })
+            .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+    }
+
+  toggleSelected(index){
+        if(this.selected[index]){
+            this.selected[index] = false;
+            this.numSelected--;
+        }
+        else{
+            if(this.numSelected === 3){
+                alert("Please only select 3 meals.");
+                return;
+            }
+            this.selected[index] = true;
+            this.numSelected++;
+        }
         this.forceUpdate();
   }
 
+  getSelectedForEmail(){
+        let selectedString = "";
+        for(let i = 0; i < this.selected.length; i++){
+            if(this.selected[i]){
+                selectedString += this.state.items[i].title + '\n';
+            }
+        }
+        return selectedString;
+  }
+
+    renderHateThisButton() {
+        return (
+            <Button variant="outlined" color="primary" onClick={() => this.handleHateSubmit()}>Please find more recipes!</Button>
+        );
+    }
+
+    handleHateSubmit(){
+        const template_params = {
+                chosen_meals: `Find more recipes!`,
+                from_name: "Meal Selection Site",
+                reply_to: "marykcarrigan@gmail.com"
+        };
+        this.sendFeedback('template_9k48ken', template_params)
+        alert("Mary has been notified she needs to find more recipes!");
+    }
 }
 
